c2bb45f0c0ca004e65cf36fefae4c287ca050fef
Abdul Aleem <engineerabdulaleem@gmail.com>
2024-05-26 16:47:44 +0500

    add jest for test

---
+++ src/tests/LoginScreen.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {render, fireEvent} from '@testing-library/react-native';
+import LoginScreen from '../screens/LoginScreen';
+
+test('shows error messages for invalid inputs', () => {
+  const {getByPlaceholderText, getByText} = render(<LoginScreen />);
+
+  fireEvent.changeText(
+    getByPlaceholderText('Enter your email'),
+    'invalidEmail',
+  );
+  fireEvent.changeText(getByPlaceholderText('Enter your password'), '123');
+  fireEvent.press(getByText('Login'));
+
+  expect(getByText('Invalid email format')).toBeTruthy();
+  expect(getByText('Password must be at least 8 characters')).toBeTruthy();
+});
+
+test('handles successful login', () => {
+  const {getByPlaceholderText, getByText, queryByText} = render(
+    <LoginScreen />,
+  );
+
+  fireEvent.changeText(
+    getByPlaceholderText('Enter your email'),
+    'test@example.com',
+  );
+  fireEvent.changeText(getByPlaceholderText('Enter your password'), 'password');
+  fireEvent.press(getByText('Login'));
+
+  expect(queryByText('Invalid email format')).toBeNull();
+  expect(queryByText('Password must be at least 6 characters')).toBeNull();
+});
